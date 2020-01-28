1f7babb3814d6a44936b987c7b5065b18de92027
SQL617 <cullerea@wit.edu>
2020-01-28 15:44:47 -0500

    Set Up Code Push
    First code push setting up ATBI repository.

---
+++ ATBI/RatingActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ATBI
+{
+    [Activity(Label = "RatingActivity")]
+    public class RatingActivity : ListActivity
+    {
+        string[] items;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            items = new string[] { "Symptom1", "Symptom2", "Symptom3", "Symptom4", "Symptom5", "Symptom6" };
+            ListAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItem1, items);
+           // ListAdaoter2 = new ArrayAdapter<SeekBar>(this,Android.Resource.Layout.sim)
+            // Create your application here
+        }
+    }
+}
---
+++ ATBI/obj/Debug/81/android/src/md57452200c814a2646f15ae4b8e2ac72d3/RatingActivity.java
@@ -0,0 +1,47 @@
+package md57452200c814a2646f15ae4b8e2ac72d3;
+
+
+public class RatingActivity
+	extends android.app.ListActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("ATBI.RatingActivity, ATBI", RatingActivity.class, __md_methods);
+	}
+
+
+	public RatingActivity ()
+	{
+		super ();
+		if (getClass () == RatingActivity.class)
+			mono.android.TypeManager.Activate ("ATBI.RatingActivity, ATBI", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
